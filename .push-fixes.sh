80f8f6a1128039a11dedf972e0d66887d97a1c0e
Owen Barton <owen@owenbarton.com>
2019-04-29 16:53:50 -0700

    Improve error message for fork PRs.

--- .push-fixes.sh
+++ .push-fixes.sh
@@ -1,13 +1,21 @@
 if [[ $(git log -1 --pretty=%B | grep -Fq 'Automatic commit') ]]; then
-  echo "Previous commit was automatic, so skip pushing fixes."
+  echo 'Previous commit was automatic, so skip pushing fixes.'
   exit 0
 fi
 
 # Run once to fix content as needed - redirect output to avoid double output
 gulp &> /dev/null
 
-# Commit and push any changes
+# Detect if there are any changes
 if [[ $(git status --porcelain) ]]; then
+  # Bail if we are working with a fork (we won't have perms to push to branches in forks)
+  if [[ "${TRAVIS_PULL_REQUEST_SLUG,,}" != "civicactions/handbook" ]]; then
+    echo "Automatic fixes doesn't work with forked repositories (only civicactions/handbook branch PRs)."
+    echo "Apply the change below and resubmit:"
+    git diff
+    exit 1
+  fi
+  # Everything looks OK, commit and push the change
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git commit -a -m"Automatic commit to fix markdown formatting for build: ${TRAVIS_BUILD_NUMBER}."
